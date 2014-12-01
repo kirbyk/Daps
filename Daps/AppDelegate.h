876e31b16e8a18b6c67290792a5eb55012e78dc7
Kirby Kohlmorgen <kirby@purdue.edu>
2014-12-01 18:34:43 -0500

    Initial Commit

---
+++ Daps/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Daps
+//
+//  Created by Kirby Kohlmorgen on 12/1/14.
+//  Copyright (c) 2014 Macrolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Daps/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Daps
+//
+//  Created by Kirby Kohlmorgen on 12/1/14.
+//  Copyright (c) 2014 Macrolab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
